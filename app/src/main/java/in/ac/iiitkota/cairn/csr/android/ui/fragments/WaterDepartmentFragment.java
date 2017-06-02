dcb67bc557dd9eda941934a8ad60708ff3020e8b
Sahil <sahilayank91>
2017-06-03 04:21:58 +0530

    Nandghar Module Completed

---
+++ app/src/main/java/in/ac/iiitkota/cairn/csr/android/ui/fragments/WaterDepartmentFragment.java
@@ -0,0 +1,104 @@
+package in.ac.iiitkota.cairn.csr.android.ui.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import in.ac.iiitkota.cairn.csr.android.R;
+import in.ac.iiitkota.cairn.csr.android.ui.WaterDepartmentDetailActivity;
+
+/**
+ * Provides UI for the view with Tile.
+ */
+public class WaterDepartmentFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        RecyclerView recyclerView = (RecyclerView) inflater.inflate(
+                R.layout.recycler_view, container, false);
+        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        // Set padding for Tiles
+        int tilePadding = getResources().getDimensionPixelSize(R.dimen.tile_padding);
+        recyclerView.setPadding(tilePadding, tilePadding, tilePadding, tilePadding);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+        return recyclerView;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView picture;
+        public TextView name;
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_tile, parent, false));
+            picture = (ImageView) itemView.findViewById(R.id.tile_picture);
+            name = (TextView) itemView.findViewById(R.id.tile_title);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Context context = v.getContext();
+                    Toast.makeText(context,"You selected the " + getAdapterPosition() + "Department",Toast.LENGTH_SHORT).show();
+                   Intent intent = new Intent(context, WaterDepartmentDetailActivity.class);
+                    intent.putExtra(WaterDepartmentDetailActivity.EXTRA_POSITION, getAdapterPosition());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    /**
+     * Adapter to display recycler view.
+     */
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        // Set numbers of Tiles in RecyclerView.
+        private static final int LENGTH = 18;
+
+        private final String[] mDepartments;
+        private final Drawable[] mPlacePictures;
+        public ContentAdapter(Context context) {
+            Resources resources = context.getResources();
+            mDepartments = resources.getStringArray(R.array.water_departments);
+            TypedArray a = resources.obtainTypedArray(R.array.water_dept_logo);
+            mPlacePictures = new Drawable[a.length()];
+            for (int i = 0; i < mPlacePictures.length; i++) {
+                mPlacePictures[i] = a.getDrawable(i);
+            }
+            a.recycle();
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.picture.setImageDrawable(mPlacePictures[position % mPlacePictures.length]);
+            holder.name.setText(mDepartments[position % mDepartments.length]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mPlacePictures.length;
+        }
+    }
+}
