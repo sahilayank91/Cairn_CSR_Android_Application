e447111f66180ace2b486cb9251589ac044ad6cf
Sahil <sahilayank91>
2017-06-03 04:21:14 +0530

    Nandghar Module Completed

---
+++ app/src/main/java/in/ac/iiitkota/cairn/csr/android/model/Fontus.java
@@ -0,0 +1,30 @@
+package in.ac.iiitkota.cairn.csr.android.model;
+
+/**
+ * Created by root on 2/6/17.
+ */
+
+public class Fontus {
+
+    private String area_name;
+    private String launch_date;
+
+
+    public void setArea_name(String area_name){
+        this.area_name = area_name;
+    }
+
+    public String getArea_name(){
+        return area_name;
+    }
+
+    public void setLaunch_date(String launch_date){
+        this.launch_date = launch_date;
+    }
+
+    public String getLaunch_date(){
+        return launch_date;
+    }
+
+
+}
